5bb40341aa57863b93d0e79bc9cdf888747e48d1
pierrepoulouin <pierrepoulouin@gmail.com>
2018-11-08 10:56:04 +0100

    debut de projet

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+
+import { AppComponent } from './app.component'
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md'
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, MDBBootstrapModule.forRoot()],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
